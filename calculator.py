29dce0dcb866396d604d6c3cd5beb9b2153af2a7
Alessandro Barbieri <afbarbieri@AlessandrosiMac.attlocal.net>
2026-04-13 19:40:18 -0400

    modified calculator

--- calculator.py
+++ calculator.py
@@ -4,18 +4,23 @@
 
 import math
 
+def square_root(a):
+    try:
+        return math.sqrt(a)
+    except ValueError as e:
+        print(e)
+
+def hypotenuse(a, b):
+    math.hypot(a, b)
 
 def add(a, b):
    return a + b
 
-
 def subtract(a, b):
    return a - b
 
-
 def multiply(a, b):
    return a * b
-
 
 def divide(a, b):
    try:
@@ -23,13 +28,11 @@ def divide(a, b):
    except ZeroDivisionError as e:
        print(e)
 
-
 def logarithm(a, b):
    try:
        return math.log(b, a)
    except ValueError as e:
        print(e)
-
 
 def exponent(a, b):
    return a ** b
