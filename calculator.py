a3ab5737b8054cf4c3f1bb4e3a08a6243f8f95df
PrabGov <mail4prabg@gmail.com>
2026-04-13 19:30:49 -0400

    modified calculator p1

---
+++ calculator.py
@@ -0,0 +1,29 @@
+# https://github.com/Mr-Neutrino/Lab11-AB-PG
+# Partner 1: Alessandro Barbieri
+# Partner 2: Prabhav Govindu
+
+import math
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    try:
+        return b / a
+    except:
+        raise ZeroDivisionError()
+
+def log(a, b):
+    try:
+        return math.log(b, a)
+    except:
+        raise ValueError()
+
+def exp(a, b):
+    return a ** b
